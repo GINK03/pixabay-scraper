2125792c2ec799334ad78c67c1c0677a5142df6c
catindog <gim.kobayashi@gmail.com>
2017-11-02 16:33:48 +0900

    update

--- google_vision.py
+++ google_vision.py
@@ -43,30 +43,40 @@ def mapper( name ):
   except Exception as e:
     print(e) 
 
+if '--scan' in sys.argv:
+  names = [name for name in glob.glob('minimize/*.jpg')]
+  #[mapper(name) for name in names#]
+  with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+    executor.map( mapper, names )
 
 if '--minimize' in sys.argv:
   from concurrent.futures import ProcessPoolExecutor
   from PIL import Image
   def _minimize(name):
-    save_name = 'minimize/' + name.split('/').pop()
-    if os.path.exists(save_name):
-      return
     try:
-      img = Image.open(name)
-    except OSError as e:
-      return
-    size = img.size
-    by = int(size[0]/1000)
-    if by != 0:
-      resize = (size[0]//by, size[1]//by)
+      save_name = 'minimize/' + name.split('/').pop()
+      if os.path.exists(save_name):
+        return
+      try:
+        img = Image.open(name)
+      except OSError as e:
+        return
+      size = img.size
+      maxx = max( [size[0], size[1]] )
+      bairitsu = size[0]//224
+      resize = (size[0]//bairitsu, size[1]//bairitsu)
       try:
         img = img.resize( resize )
       except OSError as e:
         return
-    img.save(save_name)
-    print( by, name  )
-  names = [name for name in glob.glob('./download/*')]
-  with ProcessPoolExecutor(max_workers=8) as exe:
+      img.save(save_name)
+      print( bairitus, name  )
+    except Exception as e:
+      print('Some Deep Error as', e)
+  names = [name for name in glob.glob('./imgs/*')]
+  for name in names:
+    _minimize(name)
+  with ProcessPoolExecutor(max_workers=16) as exe:
     exe.map( _minimize, names)
 
 if '--remove' in sys.argv:
@@ -76,9 +86,4 @@ if '--remove' in sys.argv:
       os.remove(name)
       print( o )
 
-if '--scan' in sys.argv:
-  names = [name for name in glob.glob('minimize/*')]
-  #[mapper(name) for name in names#]
-  with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
-    executor.map( mapper, names )
 
