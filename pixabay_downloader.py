a58941877d10f4afd1ff93a6afb38de4cc26f9ea
nardtree <gim.kobayashi@gmail.com>
2018-01-13 21:14:49 +0000

    a

--- pixabay_downloader.py
+++ pixabay_downloader.py
@@ -9,7 +9,6 @@ import argparse
 import multiprocessing as mp
 import datetime
 import pickle as pickle
-import plyvel
 import re
 import json
 import random
@@ -21,40 +20,38 @@ import http.cookiejar, random
 import re
 import shutil
 import concurrent.futures 
+import hashlib
 
 SEED_EXIST          = True
 SEED_NO_EXIST       = False
+HEADER = {"Accept-Language": "ja,en-US;q=0.9,en;q=0.8", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36","Accept": "*/*", "Connection": "keep-alive" } 
 # set default state to scrape web pages in Amazon Kindle
 def get_html(url):
-  headers = {"Accept-Language": "en-US,en;q=0.5","User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0","Accept": "*/*", "Referer": url,"Connection": "keep-alive" } 
-  request = urllib.request.Request(url=url, headers=headers)
+  request = urllib.request.Request(url=url, headers=HEADER)
   opener = urllib.request.build_opener()
   try:
     html = opener.open(request).read()
   except Exception as e:
     print('Error', e)
     return (None, None, None)
-    
   print(url)
   #print(html)
   soup = bs4.BeautifulSoup(html, "html.parser")
   title = (lambda x:str(x.string) if x != None else 'Untitled')(soup.title )
   return (html, title, soup)
 
-def get_image(url, src, tags):
-  headers = {"Accept-Language": "en-US,en;q=0.5","User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0","Accept": "*/*","Referer": url, "Connection": "keep-alive"  } 
-  request = urllib.request.Request(url=src, headers=headers)
-  request.add_header('Referer', url)
+def get_image(link, src, tags):
+  HEADER['Referer'] = link
+  request = urllib.request.Request(url=src, headers=HEADER)
+  request.add_header('Referer', link)
   opener = urllib.request.build_opener()
   con = opener.open(request).read()
-  """ illsut id """
-  linker = src.split('/').pop()
-  open('imgs/' + linker, 'wb').write(con)
-  open('metas/{}.json'.format(linker), "w").write( json.dumps({'linker':linker + '.jpg', 'tags': tags, 'url':url, 'src':src }) )
-  print("発見した画像", tags, url, src)
+  name = hashlib.sha256(bytes(link, 'utf8')).hexdigest()
+  open('imgs/{}.jpg'.format(name), 'wb').write(con)
+  open('metas/{}.json'.format(name), "w").write( json.dumps({'name':name, 'tags': tags, 'link':link, 'src':src }) )
+  print("発見した画像", tags, link, src)
 
 def analyzing(link, soup):
-
   for img in soup.find_all('img', {'itemprop': 'contentURL'}):
     src = img.get('src')
     alt = img.get('alt')
@@ -70,48 +67,29 @@ def analyzing(link, soup):
   return iternal_links
 
 def _map(link):
-  #print(link)
   try:
+    name = hashlib.sha256(bytes(link, 'utf8')).hexdigest()
+    if os.path.exists('htmls/{}'.format(name)) is True:
+      return [], []
     html, title, soup = get_html(link)
+    open('htmls/{}'.format(name), 'w').write( html.decode() )
     if soup is None:
       return link, []
     internal_links = analyzing(link, soup)
+    open('links/{}'.format(name), 'w').write( '\n'.join(internal_links) )
     return link, internal_links
   except Exception as e:
     print('Some Deep Error Occured', e)
     return link, []
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Process Kindle Referenced Index Score.')
-  parser.add_argument('--URL', help='set default URL which to scrape at first')
-  parser.add_argument('--depth', help='how number of url this program will scrape')
-  parser.add_argument('--mode', help='you can specify mode...')
-  parser.add_argument('--refresh', help='create snapshot(true|false)')
-  parser.add_argument('--file', help='input filespath')
-  parser.add_argument('--active', help='spcific active thread number')
-
-  args_obj = vars(parser.parse_args())
-  depth    = (lambda x:int(x) if x else 300)( args_obj.get('depth') )
-  mode     = (lambda x:x if x else 'undefined')( args_obj.get('mode') )
-  refresh  = (lambda x:False if x=='false' else True)( args_obj.get('refresh') )
-  active   = (lambda x:15 if x==None else int(x) )( args_obj.get('active') )
-  filename = args_obj.get('file')
-
   seed = 'https://pixabay.com/ja/%E3%83%90%E3%83%A9-%E3%83%94%E3%83%B3%E3%82%AF-%E3%83%94%E3%83%B3%E3%82%AF%E3%81%AE%E3%83%90%E3%83%A9-%E6%BA%80%E9%96%8B%E3%81%AE%E3%83%90%E3%83%A9-%E8%8A%B1-%E3%83%90%E3%83%A9%E3%81%AE%E5%92%B2%E3%81%8F-2892821/'
-  db = plyvel.DB('pixabay.ldb', create_if_missing=True)
-  db.put(bytes(seed,'utf8'), pickle.dumps(False))
-
+  links = {seed}
   while True:
-    links = []
-    for key, val in db:
-      if pickle.loads(val) == False:
-        links.append( key.decode('utf8') )
-
-    #for link in links:
-    _map(links[0])
-    with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
-      for _link, ilinks in executor.map( _map, links):
-        db.put( bytes(_link, 'utf8'), pickle.dumps(True) )
+    _links = set()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
+      for _, ilinks in executor.map( _map, links):
         for ilink in ilinks:
-          if db.get( bytes(ilink, 'utf8')) is None:
-            db.put( bytes(ilink, 'utf8'), pickle.dumps(False))
+          _links.add(ilink)
+    links = _links
+          
